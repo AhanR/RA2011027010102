61320569a672947eb23412d61759952f16bea1ea
Ahan <64554828+AhanR@users.noreply.github.com>
2023-04-14 20:01:47 +0530

    adding in server housekeeping

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-console.log("This is working")
+import express, { NextFunction, Router } from 'express';
+import http from 'http';
+
+const app = express();
+const PORT = 8080
+
+app.get("/", (req, res, next) => {
+    res.send("Hello");
+})
+
+http.createServer(app).listen(PORT, () => console.log("Server starting..."))
